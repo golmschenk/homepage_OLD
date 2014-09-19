b0ff5c57e4929673ba01da695ea5d500c5120aa2
Greg Olmschenk <golmschenk@gmail.com>
2014-09-18 20:41:09 -0400

    Move essays to a separate folder and file type

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ import jinja2
 debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 
 jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader([os.path.dirname(__file__), os.path.dirname(__file__) + "/templates"]),
-    variable_start_string="((",
-    variable_end_string="))")
+    loader=jinja2.FileSystemLoader([os.path.dirname(__file__), os.path.dirname(__file__) + "/templates"]))
 
 
 class LandingPage(webapp2.RequestHandler):
@@ -28,11 +26,12 @@ class ProposalPage(webapp2.RequestHandler):
 
 class EssayPage(webapp2.RequestHandler):
     def get(self, essay_name):
-        print essay_name + '.html'
-        import logging
-        logging.warn(essay_name + '.html')
-        template = jinja_environment.get_template('essays/' + essay_name + '.html')
-        self.response.write(template.render())
+        with open('essays/' + essay_name + '.essay') as file_handle:
+            file_lines = file_handle.read().decode('utf-8').split('\n')
+            template_variables = {'essay_title': file_lines[0],
+                                  'paragraph_list': file_lines[1:]}
+        template = jinja_environment.get_template('essay-container.html')
+        self.response.write(template.render(template_variables))
 
 
 
