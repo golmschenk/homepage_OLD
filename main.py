95f60457db55e802da3d1b0d27911f1a0ba095a0
Greg Olmschenk <golmschenk@gmail.com>
2014-09-18 21:11:54 -0400

    Add publisher and date to essay display

--- main.py
+++ main.py
@@ -28,8 +28,12 @@ class EssayPage(webapp2.RequestHandler):
     def get(self, essay_name):
         with open('essays/' + essay_name + '.essay') as file_handle:
             file_lines = file_handle.read().decode('utf-8').split('\n')
+            #Filter out blank lines.
+            file_content = [line for line in file_lines if line]
             template_variables = {'essay_title': file_lines[0],
-                                  'paragraph_list': file_lines[1:]}
+                                  'publisher': file_lines[1],
+                                  'date': file_lines[2],
+                                  'paragraph_list': file_content[3:]}
         template = jinja_environment.get_template('essay-container.html')
         self.response.write(template.render(template_variables))
 
